06492722cd585ab6e56a015d50b4f3e210db5512
bin.shen <bluelazysb@hotmail.com>
2016-09-18 15:52:40 +0800

    123123

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,18 @@
 
 module.exports = function (app, router, wrap, mongoose) {
 
+    var Data = mongoose.model('Data');
+
     router.get('/list', wrap(function* (req, res, next) {
+        var mac = req.query.mac;
+        var data = [];
+        if(mac != undefined) {
+            data = yield Data.find({mac: mac.toLowerCase() }).sort({'created': -1}).limit(10).exec()
+        } else {
+            mac = "";
+        }
         var user = req.session.login_user;
-        res.render('list_data', { page: "data", user: user });
+        res.render('list_data', { page: "data", user: user, data: data, mac: mac });
     }));
 
     app.use('/data', router);
