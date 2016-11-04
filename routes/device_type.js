045ba5aadc6244d671a5e3557ac6a67c454e1e7b
bin.shen <bluelazysb@hotmail.com>
2016-11-04 10:15:17 +0800

    updated

--- routes/device_type.js
+++ routes/device_type.js
@@ -2,6 +2,8 @@
  * Created by bin.shen on 03/11/2016.
  */
 
+//https://www.npmjs.com/package/node-xlsx
+
 module.exports = function (app, router, wrap, mongoose) {
 
     var DeviceType = mongoose.model('DeviceType');
@@ -11,7 +13,7 @@ module.exports = function (app, router, wrap, mongoose) {
         var deviceTypes = yield DeviceType.find({}).exec();
         console.log(deviceTypes);
 
-        res.render('list_device_type', { page: "device_type" });
+        res.render('list_device_type', { page: "device_type", user: req.session.login_user });
     }));
 
     app.use('/device_type', router);
